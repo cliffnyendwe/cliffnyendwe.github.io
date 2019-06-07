803d34828da6d8b41ca0ececced20e71490f9188
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2019-06-07 16:06:47 +0300

    changed the whole structure

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,30 +1,3 @@
-// $("contact-form").submit(function (e) {
-//     var firstName  = document.getElementById("inputFirstName"),
-//         lastName = document.getElementById("inputLastName"),
-//         email = document.getElementById("inputEmail"),
-//         message = document.getElementById("message"),
-//         pnoneNumber = document.getElementById("inputPhoneNumber"),
-//         city = document.getElementById("inputCity"),
-//         company = document.getElementById("inputCompany");
-
-//     if(!name.value || !firstName.value || !lastName.value || !email.value || !phoneNumber.value || !company.value || !city.value || !message.value) {
-//         alertify("please check your entries")
-//     }
-
-//     else{
-//         $.ajax({
-//             url: "https://formspree.io/cliffnyendwe2018@gmail.com",
-//             method: "POST",
-//             data: $(this).serialize(),
-//             dataType: "json"
-//           });
-//         e.preventDefault()
-//         $(this).get(0).reset()
-//         alertify.success("Message sent successfully")
-//     }
-
-// }
-// )
 (function ($) {
     'use strict';
 
