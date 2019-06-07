be5294a74be80ac3414c74fcf59a1cf2867ba70e
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2019-06-07 15:44:52 +0300

    modified my portfolio

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,27 +1,66 @@
-$("contact-form").submit(function (e) {
-    var firstName  = document.getElementById("inputFirstName"),
-        lastName = document.getElementById("inputLastName"),
-        email = document.getElementById("inputEmail"),
-        message = document.getElementById("message"),
-        pnoneNumber = document.getElementById("inputPhoneNumber"),
-        city = document.getElementById("inputCity"),
-        company = document.getElementById("inputCompany");
+// $("contact-form").submit(function (e) {
+//     var firstName  = document.getElementById("inputFirstName"),
+//         lastName = document.getElementById("inputLastName"),
+//         email = document.getElementById("inputEmail"),
+//         message = document.getElementById("message"),
+//         pnoneNumber = document.getElementById("inputPhoneNumber"),
+//         city = document.getElementById("inputCity"),
+//         company = document.getElementById("inputCompany");
 
-    if(!name.value || !firstName.value || !lastName.value || !email.value || !phoneNumber.value || !company.value || !city.value || !message.value) {
-        alertify("please check your entries")
+//     if(!name.value || !firstName.value || !lastName.value || !email.value || !phoneNumber.value || !company.value || !city.value || !message.value) {
+//         alertify("please check your entries")
+//     }
+
+//     else{
+//         $.ajax({
+//             url: "https://formspree.io/cliffnyendwe2018@gmail.com",
+//             method: "POST",
+//             data: $(this).serialize(),
+//             dataType: "json"
+//           });
+//         e.preventDefault()
+//         $(this).get(0).reset()
+//         alertify.success("Message sent successfully")
+//     }
+
+// }
+// )
+(function ($) {
+    'use strict';
+
+    var form = $('.contact__form'),
+        message = $('.contact__msg'),
+        form_data;
+
+    // Success function
+    function done_func(response) {
+        message.fadeIn().removeClass('alert-danger').addClass('alert-success');
+        message.text(response);
+        setTimeout(function () {
+            message.fadeOut();
+        }, 2000);
+        form.find('input:not([type="submit"]), textarea').val('');
     }
 
-    else{
+    // fail function
+    function fail_func(data) {
+        message.fadeIn().removeClass('alert-success').addClass('alert-success');
+        message.text(data.responseText);
+        setTimeout(function () {
+            message.fadeOut();
+        }, 2000);
+    }
+    
+    form.submit(function (e) {
+        e.preventDefault();
+        form_data = $(this).serialize();
         $.ajax({
-            url: "https://formspree.io/cliffnyendwe2018@gmail.com",
-            method: "POST",
-            data: $(this).serialize(),
-            dataType: "json"
-          });
-        e.preventDefault()
-        $(this).get(0).reset()
-        alertify.success("Message sent successfully")
-    }
-
-}
-)
+            type: 'POST',
+            url: form.attr('action'),
+            data: form_data
+        })
+        .done(done_func)
+        .fail(fail_func);
+    });
+    
+})(jQuery);
